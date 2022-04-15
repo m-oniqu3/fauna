df38c40402cda7edad19988fe962def96ec354d7
Monique McIntyre <skaiwilliams85@gmail.com>
2022-04-15 07:30:53 -0500

    added and styled navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
 import Navbar from "./components/nav/Navbar";
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Routes>
+        <Route />
+      </Routes>
     </div>
   );
 };
--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import styled from "./NavBar.module.css";
+import logo from "../../images/logo.png";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className={styled.nav}>
+      <figure className={styled.logo}>
+        <img src={logo} alt="fauna logo" />
+        <figcaption>fauna</figcaption>
+      </figure>
+
+      <ul>
+        <li>
+          <Link to="">Home</Link>
+        </li>
+        <li>
+          <Link to="">Garden</Link>
+        </li>
+      </ul>
+
+      <button className={styled.login}>Login</button>
+    </nav>
+  );
 };
 
 export default Navbar;
